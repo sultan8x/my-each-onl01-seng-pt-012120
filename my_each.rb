053f4e2bbfb830028add846ea50d8204d58e4d08
Sultan Afsheen <leadsultan8x@gmail.com>
2020-02-02 17:43:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+# words = ['hi', 'hello', 'bye', 'goodbye']
+require 'pry'
+
+def my_each(array)
+  while array.length <= 4
+    array.collect {|word| p "#{word}"}
+  end
+
+  arrat
+
 end
+
+# my_each(words)
